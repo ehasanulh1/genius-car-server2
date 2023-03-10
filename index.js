8d224859bf967c52b8c502d946dfd5c1df6087c3
ehasanulh1 <ehasanulh1@gmail.com>
2023-03-11 01:14:57 +0600

    dotenv

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 // const jwt = require('jsonwebtoken');
 // const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
