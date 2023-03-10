afcee1b4094f25b1c07c991bb4edf63f5552efb9
ehasanulh1 <ehasanulh1@gmail.com>
2023-03-11 01:20:20 +0600

    added mongodb client

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 // const jwt = require('jsonwebtoken');
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -10,9 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w1kojzp.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri)
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w1kojzp.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 // function verifyJwt(req, res, next) {
