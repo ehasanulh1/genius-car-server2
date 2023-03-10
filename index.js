630b039fc11a8f1f17256d549db2d565f0a52167
ehasanulh1 <ehasanulh1@gmail.com>
2023-03-11 01:27:02 +0600

    run fnc

--- index.js
+++ index.js
@@ -31,82 +31,82 @@ app.use(cors());
 // }
 
 
-// async function run() {
-//     try {
-//         const serviceCollection = client.db('geniusCar').collection('services');
-//         const orderCollection = client.db('geniusCar').collection('orders');
+async function run() {
+    try {
+        //         const serviceCollection = client.db('geniusCar').collection('services');
+        //         const orderCollection = client.db('geniusCar').collection('orders');
 
-//         app.post('/jwt', (req, res) => {
-//             const user = req.body;
-//             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-//             res.send({ token })
-//         })
+        //         app.post('/jwt', (req, res) => {
+        //             const user = req.body;
+        //             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+        //             res.send({ token })
+        //         })
 
-//         app.get('/services', async (req, res) => {
-//             const query = {};
-//             const cursor = serviceCollection.find(query);
-//             const services = await cursor.toArray();
-//             res.send(services);
-//         });
+        //         app.get('/services', async (req, res) => {
+        //             const query = {};
+        //             const cursor = serviceCollection.find(query);
+        //             const services = await cursor.toArray();
+        //             res.send(services);
+        //         });
 
-//         app.get('/services/:id', async (req, res) => {
-//             const id = req.params.id;
-//             const query = { _id: new ObjectId(id) };
-//             const result = await serviceCollection.findOne(query);
-//             res.send(result);
-//         });
+        //         app.get('/services/:id', async (req, res) => {
+        //             const id = req.params.id;
+        //             const query = { _id: new ObjectId(id) };
+        //             const result = await serviceCollection.findOne(query);
+        //             res.send(result);
+        //         });
 
-//         // orders api
-//         app.get('/orders', verifyJwt, async (req, res) => {
-//             const decoded = req.decoded;
-//             if (decoded.email !== req.query.email) {
-//                 res.status(403).send({ message: 'unauthorized access' })
-//             }
-//             let query = {};
-//             if (req.query.email) {
-//                 query = {
-//                     email: req.query.email
-//                 }
-//             }
+        //         // orders api
+        //         app.get('/orders', verifyJwt, async (req, res) => {
+        //             const decoded = req.decoded;
+        //             if (decoded.email !== req.query.email) {
+        //                 res.status(403).send({ message: 'unauthorized access' })
+        //             }
+        //             let query = {};
+        //             if (req.query.email) {
+        //                 query = {
+        //                     email: req.query.email
+        //                 }
+        //             }
 
-//             const cursor = orderCollection.find(query);
-//             const order = await cursor.toArray();
-//             res.send(order);
-//         })
+        //             const cursor = orderCollection.find(query);
+        //             const order = await cursor.toArray();
+        //             res.send(order);
+        //         })
 
-//         app.post('/orders', async (req, res) => {
-//             const order = req.body;
-//             const result = await orderCollection.insertOne(order);
-//             res.send(result);
-//         });
+        //         app.post('/orders', async (req, res) => {
+        //             const order = req.body;
+        //             const result = await orderCollection.insertOne(order);
+        //             res.send(result);
+        //         });
 
-//         app.patch('/orders/:id', async (req, res) => {
-//             const id = req.params.id;
-//             const status = req.params.status;
-//             const query = { _id: new ObjectId(id) };
-//             const updatedDoc = {
-//                 $set: {
-//                     status: status
-//                 }
-//             }
-//             const result = await orderCollection.updateOne(query, updatedDoc)
-//             res.send(result);
-//         })
+        //         app.patch('/orders/:id', async (req, res) => {
+        //             const id = req.params.id;
+        //             const status = req.params.status;
+        //             const query = { _id: new ObjectId(id) };
+        //             const updatedDoc = {
+        //                 $set: {
+        //                     status: status
+        //                 }
+        //             }
+        //             const result = await orderCollection.updateOne(query, updatedDoc)
+        //             res.send(result);
+        //         })
 
-//         app.delete('/orders/:id', async (req, res) => {
-//             const id = req.params.id;
-//             const query = { _id: new ObjectId(id) };
-//             const result = await orderCollection.deleteOne(query);
-//             res.send(result);
-//         })
+        //         app.delete('/orders/:id', async (req, res) => {
+        //             const id = req.params.id;
+        //             const query = { _id: new ObjectId(id) };
+        //             const result = await orderCollection.deleteOne(query);
+        //             res.send(result);
+        //         })
 
 
-//     }
-//     finally {
+    }
+    finally {
 
-//     }
-// }
-// run().catch(err => console.error(err));
+    }
+}
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('Hello from genius car server')
